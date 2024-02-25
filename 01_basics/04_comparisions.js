71bf5b206afde434b1e03338acde14dc2c8f5c62
Jatinchhabra22 <147986920+Jatinchhabra22@users.noreply.github.com>
2024-02-25 22:57:32 +0530

    comparisons

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,10 @@
+
+console.log(2>1);
+console.log(2 == 2);
+console.log(1 <= 1);
+console.log("2" > 1);
+console.log(2 > "1");
+
+// === vala check 
+
+console.log("2" === 2); //=== checks the dataType also
