263d6cf15c25518a06062ac6eb990fbe6233658a
Jatinchhabra22 <147986920+Jatinchhabra22@users.noreply.github.com>
2024-07-03 12:49:19 +0530

    cmt added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "jatin@google"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur" // not a good method
 let accountState;
 
 // accountId = 2 // not allowed 
