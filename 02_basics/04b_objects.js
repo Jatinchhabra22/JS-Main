603555893c45156aad861eaa239599e6c9267312
Jatinchhabra22 <147986920+Jatinchhabra22@users.noreply.github.com>
2024-07-05 18:08:09 +0530

    objects

---
+++ 02_basics/04b_objects.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // ab baar baar poora nhi likhna padega sirf 
+// intsructor likhne se kaam ho jaega 
+
+// console.log(courseInstructor);
+console.log(instructor);
+
